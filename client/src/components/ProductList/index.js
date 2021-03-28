e04afbccc0123e76329290850982563403b580ed
Shaggy <simmons5family@gmail.com>
2021-03-28 13:08:12 -0700

    passing all tests

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,33 +1,31 @@
 import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
 
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+
 import ProductItem from "../ProductItem";
-import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
-
   const { currentCategory } = state;
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if(data) {
+    if (data) {
       dispatch({
-           type: UPDATE_PRODUCTS,
-          products: data.products
-        });
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
     }
   }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return state.products
     }
-
     return state.products.filter(product => product.category._id === currentCategory);
   }
 
